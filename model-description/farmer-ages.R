5b25327c7a5dd9ef2300cbb5a5c0974994d9b630
DOSull <david@southosullivan.com>
2025-02-15 14:08:31 +1300

    more detail added to farmer age distribution docs and pages changed to 'description' not 'documentation

---
+++ model-description/farmer-ages.R
@@ -0,0 +1,31 @@
+library(extraDistr)
+library(ggplot2)
+library(dplyr)
+
+set.seed(1)
+
+get_age_df <- function(num_farmers = 250,
+                       age_range = 70,
+                       alpha = 5,
+                       beta = 4, 
+                       min_age = 20) {
+  age_draw <- data.frame(Age = 20 + rbbinom(num_farmers, age_range, alpha, beta)) |>
+    count(Age)
+  data.frame(Age = min_age + 0:age_range) |>
+    left_join(age_draw) |>
+    mutate(n = if_else(is.na(n), 0, n),
+           pdf = num_farmers * dbbinom(0:age_range, age_range, alpha, beta)) |>
+    rename(`Number of farmers` = n)
+}
+
+ages_init <- get_age_df()
+ggplot(ages_init) +
+  geom_col(aes(x = Age, y = `Number of farmers`), fill = "grey") +
+  geom_line(aes(x = Age, y = pdf), colour = "red") +
+  theme_minimal()
+
+ages_succession <- get_age_df(age_range = 45, alpha = 4, beta = 2)
+ggplot(ages_succession) +
+  geom_col(aes(x = Age, y = `Number of farmers`), fill = "grey") +
+  geom_line(aes(x = Age, y = pdf), colour = "red") +
+  theme_minimal()
